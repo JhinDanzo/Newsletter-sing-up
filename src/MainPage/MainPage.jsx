a5983b94b277a19e0519ae0c296962ddf7b6fc8e
JhinDanzo <gentj03@icloud.com>
2024-07-04 19:01:10 +0300

    markup

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,7 +1,26 @@
 
 
 function MainPage(){
-    return(<div></div>)
+    return(
+        <body>
+            <main>
+                <div>
+                    <h1></h1>
+                    <p></p>
+                    <ul className="features">
+                        <li className="features_item"></li>
+                        <li className="features_item"></li>
+                        <li className="features_item"></li>
+                    </ul>
+                    <span></span>
+                    <input></input>
+                    <button></button>
+                </div>
+                <img/>
+            </main>
+
+        </body>
+    )
 }
 
 export default MainPage;
