4d4a3450cce0ea5c30aaf07fd2be56a0a7a16583
JhinDanzo <gentj03@icloud.com>
2024-07-05 14:59:10 +0300

    email cheking form

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,13 +1,37 @@
 import IllustrationDesktop from "../assets/illustration-sign-up-desktop.svg"
 import "./MainPage.scss"
+import { useState } from "react";
 
 function MainPage(){
+
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+
+    const validateEmail = (email) => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log("no issue")
+        if (!validateEmail(email)) {
+            setError('Valid email required');
+          } else {
+            setError('');
+            console.log('email valid:', email);
+          }
+    }
+
     return(
-        <body>
+        <div className="body">
             <main>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
                 <section className="content-container">
                     <h1 className="title">Stay updated!</h1>
                     <p className="main-text">Join 60,000+ product managers receiving monthly updates on:</p>
@@ -16,16 +40,24 @@ function MainPage(){
                         <li className="features-item">Measuring to ensure updates are a success</li>
                         <li className="features-item">And much more!</li>
                     </ul>
-                    <div className="input-form">
+                    <form className="input-form" onSubmit={handleSubmit} >
                         <span className="email">Email address</span>
-                        <input className="email-input" placeholder="email@company.com"></input>
-                    </div>
-                    <button className="submit-button">Subscribe to monthly newsletter</button>
+                        {error && <span className="error-message">{error}</span>}
+                        <div>
+                            <input  type="text"
+                                    placeholder="email@company.com" 
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    className={error ? 'email-input input-error' : 'email-input'} 
+                                    /> 
+                            <button className="submit-button" type="submit">Subscribe to monthly newsletter</button>
+                        </div>
+                    </form> 
                 </section>
                 <img alt="main-image" className="main-image" src={IllustrationDesktop} />
             </main>
 
-        </body>
+        </div>
     )
 }
 
