b769ed824fb98a5c84d860f74623bcf32bb6cf87
JhinDanzo <gentj03@icloud.com>
2024-07-11 12:14:05 +0300

    mobile

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import IllustrationDesktop from "../assets/illustration-sign-up-desktop.svg"
+import IllustrationMobile from '../assets/illustration-sign-up-mobile.svg'
 import "./MainPage.scss"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -28,12 +29,13 @@ function MainPage(){
     return(
         <div className="body">
             <main>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-            <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"></link>
+                <img alt="main-image-mobile" className="main-image-mobile" src={IllustrationMobile}></img>
                 <section className="content-container">
                     <h1 className="title">Stay updated!</h1>
                     <p className="main-text">Join 60,000+ product managers receiving monthly updates on:</p>
